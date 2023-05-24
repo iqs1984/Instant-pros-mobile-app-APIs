a8313f84d1caabc295ef1521a19686215214550b
NitiniQuincesoft <nitin.shakya@iquincesoft.com>
2023-05-24 15:45:20 +0530

    auth login
    auth login complete

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -57,7 +57,34 @@ class AuthController extends Controller
 
     }
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+           
+        ]);
 
+        if($validator->fails()){
+            return response()->json($validator->errors(),422);
+        }
+
+        if(!$token = auth()->attempt($validator->validated())){
+            return response()->json(['error' => 'Unauthorized'],401);
+            
+        }
+
+        return $this->createNewToken($token);
+    }
+
+    public function createNewToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expire_in' => auth()->factory()->getTTL()*60,
+            'user'=>auth()->user()
+        ]);
     }
 }
